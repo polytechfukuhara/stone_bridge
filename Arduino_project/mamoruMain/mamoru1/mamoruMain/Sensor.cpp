5e450df8d33fc088db23bcf976b0d0382e4e8313
kumikomi22 <nino2hiro2@gmail.com>
2023-03-10 13:29:56 +0900

    ①MAINにサーボクラスservo.cpp、servo.hを作成（中身はなし。大城さんが完成していれば入力） ②リッチさんのButtonのコードを入力 ③斎藤のSensorのコードを入力 ④SUBクラスの不必要なクラスを削除（Motor,Sensor,wifiクラスを削除）

--- Arduino_project/mamoruMain/mamoru1/mamoruMain/Sensor.cpp
+++ Arduino_project/mamoruMain/mamoru1/mamoruMain/Sensor.cpp
@@ -14,3 +14,37 @@
 #include"Blutooth.h"
 #include"connectWifi.h"
 #include"Sleep_mode.h"
+
+
+
+
+
+
+
+
+const float Vref = 3.3;
+const int ADC_PIN = GPIO_NUM_33;
+void setup() {
+    M5.begin();
+    M5.Axp.ScreenBreath(9);
+    M5.Lcd.setRotation(3);
+    M5.Lcd.fillScreen(WHITE);
+    M5.Lcd.setTextColor(BLACK);
+    M5.Lcd.setTextSize(6);
+    pinMode(ADC_PIN, INPUT);
+    gpio_pulldown_dis(GPIO_NUM_25); // Disable pull-down on GPIO.
+    gpio_pullup_dis(GPIO_NUM_25); // Disable pull-up on GPIO.
+    Serial.begin(115200);
+}
+void loop() {
+    M5.Lcd.fillScreen(WHITE);
+    int val = analogRead(ADC_PIN);  // read the input pin
+    float volt = Vref * float(val) / 4095.0; // 12bit
+    Serial.println(volt);
+    if(volt < 0.5){
+      M5.Lcd.setCursor(20, 40);
+      M5.Lcd.printf("MOVE");
+      delay(1000);
+    }
+    //M5.Lcd.printf("%.2fV", volt);
+}
